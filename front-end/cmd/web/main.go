aa1bc7e3f4f6154475d540d6da7037f08e86565b
Kenzo <guilhermekenzo01@gmail.com>
2024-05-15 16:38:06 -0300

    feat: path to templates as env var

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -5,12 +5,15 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		render(w, "test.page.gohtml")
 	})
+
+	fmt.Print("teste console")
 
 	fmt.Println("Starting front end service on port 80")
 	err := http.ListenAndServe(":80", nil)
@@ -21,14 +24,19 @@ func main() {
 
 func render(w http.ResponseWriter, t string) {
 
+	htmlTemplatePath, ok := os.LookupEnv("HTML_TEMPLATES_PATH")
+	if !ok {
+		htmlTemplatePath = "./cmd/web/templates/"
+	}
+
 	partials := []string{
-		"./cmd/web/templates/base.layout.gohtml",
-		"./cmd/web/templates/header.partial.gohtml",
-		"./cmd/web/templates/footer.partial.gohtml",
+		htmlTemplatePath + "/base.layout.gohtml",
+		htmlTemplatePath + "/header.partial.gohtml",
+		htmlTemplatePath + "/footer.partial.gohtml",
 	}
 
 	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+	templateSlice = append(templateSlice, fmt.Sprintf(htmlTemplatePath+"/%s", t))
 
 	for _, x := range partials {
 		templateSlice = append(templateSlice, x)
